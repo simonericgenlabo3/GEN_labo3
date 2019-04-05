bafbf790382d7d605ea2a6a75257e71867486fd9
Simon Walther <simon.walt@hotmail.com>
2019-04-05 11:39:12 +0200

    src

---
+++ src/test/java/simonericgenlabo3/SquareTest.java
@@ -0,0 +1,14 @@
+package simonericgenlabo3;
+
+import org.junit.Test;
+import static junit.framework.TestCase.*;
+
+/**
+ * Unit test for squares
+ */
+public class SquareTest {
+    @Test
+    public void square() {
+
+    }
+}
