4a6f85a14a86d85df551cb0f5117edc749355e3d
Eric Noel <eric.noel@heig-vd.ch>
2019-04-12 11:01:18 +0200

    begin Piece

---
+++ src/test/java/simonericgenlabo3/PieceTest.java
@@ -0,0 +1,11 @@
+package simonericgenlabo3;
+
+import org.junit.jupiter.api.BeforeAll;
+
+public class PieceTest {
+
+    @BeforeAll
+    public void PieceCreationTest(){
+        //Piece piece = new Piece("test");
+    }
+}
