cbcfe08c216f0dde4fbc289d0677d7eb1c452a6a
Eric Noel <eric.noel@heig-vd.ch>
2019-04-12 11:14:36 +0200

    Piece + PieceTest

--- src/test/java/simonericgenlabo3/PieceTest.java
+++ src/test/java/simonericgenlabo3/PieceTest.java
@@ -1,11 +1,26 @@
 package simonericgenlabo3;
 
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class PieceTest {
 
     @BeforeAll
     public void PieceCreationTest(){
-        //Piece piece = new Piece("test");
+        Piece piece = new Piece("test", new Square("1"));
+        assertNotNull(piece);
+    }
+
+    @Test
+    public void PieceGetSetLocationTest(){
+        Square square1 = new Square("1");
+        Square square2 = new Square("2");
+        Piece piece = new Piece("test", square1);
+        assertEquals(piece.getLocation().getName(), square1.getName());
+        piece.setLocation(square2);
+        assertEquals(piece.getLocation().getName(), square2.getName());
     }
 }
