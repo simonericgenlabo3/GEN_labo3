e04c1a1da5b73a52b22d207f8d7001026a443d58
Eric Noel <eric.noel@heig-vd.ch>
2019-04-29 15:31:30 +0200

    PlayerTest

--- src/test/java/simonericgenlabo3/PlayerTest.java
+++ src/test/java/simonericgenlabo3/PlayerTest.java
@@ -3,6 +3,7 @@ package simonericgenlabo3;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PlayerTest {
@@ -14,5 +15,15 @@ public class PlayerTest {
         Board board = new Board();
         Player player = new Player("player one", piece, dice, board);
         assertNotNull(player);
+    }
+
+    @Test
+    public void playerShouldMoveAfterATurn(){
+        Piece piece = new Piece("test", new Square("Go"));
+        Die dice [] = {new Die(), new Die()};
+        Board board = new Board();
+        Player player = new Player("player one", piece, dice, board);
+        player.takeTurn();
+        assertNotEquals("Go", piece.getLocation().getName());
     }
 }
