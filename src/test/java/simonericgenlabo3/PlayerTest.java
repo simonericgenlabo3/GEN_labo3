a08253756fc66dd2e3eb3b9c6c58ce94b0c07628
Eric Noel <eric.noel@heig-vd.ch>
2019-05-01 09:23:34 +0200

    Mockito test of Player.takeTurn

--- src/test/java/simonericgenlabo3/PlayerTest.java
+++ src/test/java/simonericgenlabo3/PlayerTest.java
@@ -3,8 +3,10 @@ package simonericgenlabo3;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 public class PlayerTest {
 
@@ -20,10 +22,12 @@ public class PlayerTest {
     @Test
     public void playerShouldMoveAfterATurn(){
         Piece piece = new Piece("test", new Square("Go"));
-        Die dice [] = {new Die(), new Die()};
+        Die die1 = mock(Die.class);
+        when(die1.getFaceValue()).thenReturn(3);
+        Die dice [] = {die1, die1};
         Board board = new Board();
         Player player = new Player("player one", piece, dice, board);
         player.takeTurn();
-        assertNotEquals("Go", piece.getLocation().getName());
+        assertEquals("Square 6", piece.getLocation().getName());
     }
 }
